46e4f175e9a154601d644eb6afc6350747ee65b9
Yan Guokun <498821389@qq.com>
2020-05-05 21:25:26 +0800

    header组件动态获取数据

--- src/containers/Admin/Header/Header.jsx
+++ src/containers/Admin/Header/Header.jsx
@@ -7,15 +7,23 @@ import {
 } from '@ant-design/icons';
 import screenfull  from 'screenfull'
 import {connect} from 'react-redux'
+import dayjs from 'dayjs'
 import {deleteUserInfo} from '@/redux/actions/login'
+import {reqWeatherData} from '@/api'
 
-import demo from './demo.jpg'
 import './css/Header.less'
+// import { wait } from '@testing-library/react';
 const { confirm } = Modal;
 
+@connect(
+  state => ({username: state.userInfo.user.username}), //映射状态
+  {deleteUserInfo} //映射操作状态的方法
+)
 class Header extends Component {
   state = {
-    isFull: false //标识是否全屏
+    isFull: false, //标识是否全屏
+    time: dayjs().format('YYYY年MM月DD日 HH:mm:ss'), //时间
+    weatherData:{} //天气信息对象
   }
 
   //退出登录
@@ -37,17 +45,34 @@ class Header extends Component {
     screenfull.toggle(); //切换全屏
   }
 
+  getWeather = async ()=>{
+    let result = await reqWeatherData()
+    const {dayPictureUrl,weather,temperature} = result
+    this.setState({weatherData:{dayPictureUrl,weather,temperature}})
+  }
+
   componentDidMount(){
     //检测屏幕的变化
     screenfull.onchange(()=>{
       const {isFull} = this.state
       this.setState({isFull:!isFull})
     })
+    //开启一个定时器计算时间
+    this.timer = setInterval(() => {
+      this.setState({time:dayjs().format('YYYY年MM月DD日 HH:mm:ss')})
+    }, 1000); 
+    //请求天气信息
+    // this.getWeather() //避免在生命钩子函数前使用async
+    // reqWeatherData()
   }
+
+  componentWillUnmount(){
+		clearInterval(this.timer)
+	}
 
   render() {
     const {username} = this.props
-    const {isFull} = this.state
+    const {isFull,time,weatherData} = this.state
     return (
       <div className="header">
         <div className="header-top">
@@ -63,9 +88,12 @@ class Header extends Component {
             <span>首页</span>
           </div>
           <div className="bottom-right">
-            <img src={demo} alt=""/>
-            <span>2020年5月4日 00:00:00</span>
-            <span>温度：0~15℃</span>
+            {/* <img src={weatherData.dayPictureUrl} alt="天气logo"/> */}
+            <img src="http://api.map.baidu.com/images/weather/day/qing.png" alt="天气logo"/>
+            <span>{time}</span>&nbsp;
+            <span>{weatherData.weather}</span>&nbsp;
+            {/* <span>温度：{weatherData.temperature}</span> */}
+            <span>温度：1~10℃</span>
           </div>
         </div>
       </div>
@@ -73,7 +101,4 @@ class Header extends Component {
   }
 }
 
-export default connect(
-  state => ({username: state.userInfo.user.username}), //映射状态
-  {deleteUserInfo} //映射操作状态的方法
-)(Header)
+export default Header
