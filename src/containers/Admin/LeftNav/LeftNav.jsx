f250812ba79079f46263d8af157ef8e091665057
Yan Guokun <498821389@qq.com>
2020-05-05 22:59:05 +0800

    左侧导航递归生成菜单

--- src/containers/Admin/LeftNav/LeftNav.jsx
+++ src/containers/Admin/LeftNav/LeftNav.jsx
@@ -1,18 +1,33 @@
 import React, { Component } from 'react'
 import { Menu } from 'antd';
-import {
-  AppstoreOutlined,
-  PieChartOutlined,
-  DesktopOutlined,
-  ContainerOutlined,
-  MailOutlined,
-} from '@ant-design/icons';
 import logo from '@/assets/images/logo.png'
+import menus from '@/config/menu_config'
+
 import './css/left_nav.less'
 
-const { SubMenu } = Menu;
+const { SubMenu,Item } = Menu;
 
 export default class LeftNav extends Component {
+  //自动生成导航菜单
+  createMenu = (menuArr)=>{
+    return menuArr.map((menuObj)=>{
+      if(!menuObj.children){
+        return(
+          <Item key={menuObj.key} icon={<menuObj.icon />}>
+            {menuObj.title}
+          </Item>
+        )
+      }else{
+        return(
+          <SubMenu key={menuObj.key} icon={<menuObj.icon />} title={menuObj.title}>
+            {/* 递归调用完成子菜单遍历 */}
+            {this.createMenu(menuObj.children)}
+          </SubMenu>
+        )
+      }
+    })
+  }
+
   render() {
     return (
       <div className="left-nav">
@@ -20,39 +35,15 @@ export default class LeftNav extends Component {
           <img src={logo} alt=""/>
           <span>商品管理系统</span>
         </div>
-        <div>
-        <Menu
-          defaultSelectedKeys={['1']}
-          defaultOpenKeys={['sub1']}
-          mode="inline"
-          theme="dark"
-        >
-          <Menu.Item key="1" icon={<PieChartOutlined />}>
-            Option 1
-          </Menu.Item>
-          <Menu.Item key="2" icon={<DesktopOutlined />}>
-            Option 2
-          </Menu.Item>
-          <Menu.Item key="3" icon={<ContainerOutlined />}>
-            Option 3
-          </Menu.Item>
-          <SubMenu key="sub1" icon={<MailOutlined />} title="Navigation One">
-            <Menu.Item key="5">Option 5</Menu.Item>
-            <Menu.Item key="6">Option 6</Menu.Item>
-            <Menu.Item key="7">Option 7</Menu.Item>
-            <Menu.Item key="8">Option 8</Menu.Item>
-          </SubMenu>
-          <SubMenu key="sub2" icon={<AppstoreOutlined />} title="Navigation Two">
-            <Menu.Item key="9">Option 9</Menu.Item>
-            <Menu.Item key="10">Option 10</Menu.Item>
-            <SubMenu key="sub3" title="Submenu">
-              <Menu.Item key="11">Option 11</Menu.Item>
-              <Menu.Item key="12">Option 12</Menu.Item>
-            </SubMenu>
-          </SubMenu>
+          {/* antd的Menu组件 */}
+          <Menu
+            defaultSelectedKeys={['1']} //默认选中哪个菜单
+            defaultOpenKeys={['sub1']} //默认展开哪个菜单
+            mode="inline" //菜单的模式
+            theme="dark" //主题颜色
+          >
+          {this.createMenu(menus)}
         </Menu>
-      </div>
-    );
       </div>
     )
   }
