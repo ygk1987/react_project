7d4ff945d1e368d5856a522b875eac057df6ffc2
Yan Guokun <498821389@qq.com>
2020-05-06 20:56:00 +0800

    左侧导航/Header配合并通过redux做交互

--- src/containers/Admin/Header/Header.jsx
+++ src/containers/Admin/Header/Header.jsx
@@ -16,7 +16,10 @@ import './css/Header.less'
 const { confirm } = Modal;
 
 @connect(
-  state => ({username: state.userInfo.user.username}), //映射状态
+  state => ({//映射状态
+    username: state.userInfo.user.username,
+    title: state.title
+  }), 
   {deleteUserInfo} //映射操作状态的方法
 )
 class Header extends Component {
@@ -85,7 +88,7 @@ class Header extends Component {
 
         <div className="header-bottom">
           <div className="bottom-left">
-            <span>首页</span>
+            <span>{this.props.title}</span>
           </div>
           <div className="bottom-right">
             {/* <img src={weatherData.dayPictureUrl} alt="天气logo"/> */}
--- src/containers/Admin/LeftNav/LeftNav.jsx
+++ src/containers/Admin/LeftNav/LeftNav.jsx
@@ -1,21 +1,32 @@
 import React, { Component } from 'react'
 import { Menu } from 'antd';
 import {Link,withRouter} from 'react-router-dom'
+import {connect} from 'react-redux'
+import {saveTitle} from '@/redux/actions/title'
 import logo from '@/assets/images/logo.png'
 import menus from '@/config/menu_config'
 
 import './css/left_nav.less'
 
 const { SubMenu,Item } = Menu;
-
+@connect(
+  ()=>{}, //映射状态
+  {saveTitle} //映射操作状态的方法
+)
 @withRouter
 class LeftNav extends Component {
+
+  saveTitle = (title)=>{
+    console.log(title);
+    this.props.saveTitle(title)
+  }
+
   //自动生成导航菜单
   createMenu = (menuArr)=>{
     return menuArr.map((menuObj)=>{
       if(!menuObj.children){
         return(
-          <Item key={menuObj.key}>
+          <Item key={menuObj.key} onClick={()=>{this.saveTitle(menuObj.title)}}>
             <Link to={menuObj.path} style={{color:'white'}}>
               <menuObj.icon />
               {menuObj.title}
@@ -39,12 +50,13 @@ class LeftNav extends Component {
   }
 
   render() {
+    //获取路径，无论是展开还是选中，都是从路径中获取的。
     const {pathname} = this.props.location
-    const openedKey = pathname.split('/')
-    const checkedKey = openedKey.slice(-1)
+    const openedKey = pathname.split('/') //要展开的菜单
+    const checkedKey = openedKey.slice(-1) //要选中的菜单
     
     return (
-      <div className="left-nav">
+      <div className="left-nav"></div>
         <div className="nav-top">
           <img src={logo} alt=""/>
           <span>商品管理系统</span>
