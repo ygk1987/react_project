79cd7016e8c39b3eda4cc979b914f9cf1717a422
Yan Guokun <498821389@qq.com>
2020-05-06 19:44:25 +0800

    点击导航实现跳转

--- src/containers/Admin/LeftNav/LeftNav.jsx
+++ src/containers/Admin/LeftNav/LeftNav.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Menu } from 'antd';
+import {Link,withRouter} from 'react-router-dom'
 import logo from '@/assets/images/logo.png'
 import menus from '@/config/menu_config'
 
@@ -7,19 +8,28 @@ import './css/left_nav.less'
 
 const { SubMenu,Item } = Menu;
 
-export default class LeftNav extends Component {
+@withRouter
+class LeftNav extends Component {
   //自动生成导航菜单
   createMenu = (menuArr)=>{
     return menuArr.map((menuObj)=>{
       if(!menuObj.children){
         return(
-          <Item key={menuObj.key} icon={<menuObj.icon />}>
-            {menuObj.title}
+          <Item key={menuObj.key}>
+            <Link to={menuObj.path} style={{color:'white'}}>
+              <menuObj.icon />
+              {menuObj.title}
+            </Link>
           </Item>
         )
       }else{
         return(
-          <SubMenu key={menuObj.key} icon={<menuObj.icon />} title={menuObj.title}>
+          <SubMenu 
+            style={{color:'white'}}
+            key={menuObj.key} 
+            icon={<menuObj.icon />} 
+            title={menuObj.title}
+          >
             {/* 递归调用完成子菜单遍历 */}
             {this.createMenu(menuObj.children)}
           </SubMenu>
@@ -29,6 +39,10 @@ export default class LeftNav extends Component {
   }
 
   render() {
+    const {pathname} = this.props.location
+    const checkedKey = pathname.split('/').slice(-1)
+    console.log(checkedKey);
+    
     return (
       <div className="left-nav">
         <div className="nav-top">
@@ -37,8 +51,8 @@ export default class LeftNav extends Component {
         </div>
           {/* antd的Menu组件 */}
           <Menu
-            defaultSelectedKeys={['1']} //默认选中哪个菜单
-            defaultOpenKeys={['sub1']} //默认展开哪个菜单
+            selectedKeys={checkedKey} //默认选中哪个菜单
+            defaultOpenKeys={[]} //默认展开哪个菜单
             mode="inline" //菜单的模式
             theme="dark" //主题颜色
           >
@@ -48,3 +62,4 @@ export default class LeftNav extends Component {
     )
   }
 }
+export default LeftNav
