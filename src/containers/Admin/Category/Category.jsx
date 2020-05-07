79b41d734d2ca2ff940ac7191ba9d8e4b65df58e
Yan Guokun <498821389@qq.com>
2020-05-07 08:17:16 +0800

    分类组件新增Modal框

--- src/containers/Admin/Category/Category.jsx
+++ src/containers/Admin/Category/Category.jsx
@@ -1,14 +1,10 @@
 import React, { Component } from 'react'
-import { Card,Button,Table } from 'antd';
+import { Card,Button,Table,Modal,Input,Form } from 'antd';
 import {PlusCircleOutlined} from '@ant-design/icons';
 import {connect} from 'react-redux'
-
-// import {reqCategoryList} from '@/api'
-//同步action
-// import {saveCategory} from '@/redux/actions/category'
-//异步action
 import {saveCategoryAsync} from '@/redux/actions/category'
 
+const {Item} = Form 
 
 @connect(
   state =>({categorylist: state.categorylist}), //映射操作状态
@@ -16,6 +12,27 @@ import {saveCategoryAsync} from '@/redux/actions/category'
   {saveCategoryAsync}
 )
 class Category extends Component {
+  //默认不展示弹窗
+  state = { visible: false };
+  //展示弹窗
+  showModal = () => {
+    this.setState({
+      visible: true,
+    });
+  };
+  //确认的回调
+  handleOk = () => {
+    this.setState({
+      visible: false,
+    });
+  };
+  //取消的回调
+  handleCancel = () => {
+    this.setState({
+      visible: false,
+    });
+  };
+  //生命周期钩子函数
   componentDidMount(){
     this.props.saveCategoryAsync()
   }
@@ -39,18 +56,45 @@ class Category extends Component {
     ];
     return (
       <div>
-      <Card extra={<Button type="primary"><PlusCircleOutlined />添加</Button>}>
-        <Table 
-          dataSource={dataSource} //配置数据源
-          columns={columns} //配置列
-          bordered //展示边框
-          rowKey="_id" //配置唯一标识
-          pagination={{ //分页器
-            pageSize:4 //每页展示多少条
-          }} 
-        />
+        {/* Card展示组件 */}
+        <Card 
+          extra={
+            <Button type="primary" onClick={this.showModal}>
+              <PlusCircleOutlined />添加
+            </Button>
+          }
+        >
+          <Table 
+            dataSource={dataSource} //配置数据源
+            columns={columns} //配置列
+            bordered //展示边框
+            rowKey="_id" //配置唯一标识
+            pagination={{ //分页器
+              pageSize:4 //每页展示多少条
+            }} 
+          />
       </Card>
-    </div>
+        {/* Modal弹窗组件 */}
+        <Modal
+          title="新增分类" //弹窗标题
+          visible={this.state.visible} //控制弹窗是否展示
+          onOk={this.handleOk} //确认的回调
+          onCancel={this.handleCancel} //取消的回调
+          okText="确定"
+          cancelText="取消"
+        >
+          <Form>
+            <Item
+              name="category"
+              rules={[
+                {required:true, message:'分类名必须输入'}
+              ]}
+            >
+              <Input placeholder="请输入分类名"/>
+            </Item>
+          </Form>
+        </Modal>
+      </div>
     )
   }
 }
